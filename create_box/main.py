7335fb7d3239b79d0d6b748bc32d74336760cc6b
DRogers <dustinxrogers@gmail.com>
2017-01-14 15:28:05 +0000

    Added check for width/height being > 0

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,12 @@
 
 
 def create_box(height, width, character):
-    pass
-
+    string =''
+    for i in range(height):
+        for x in range(width):
+            string += character
+        string += '\n'
+    return string
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
